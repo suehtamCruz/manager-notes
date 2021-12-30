bd9becebe3f7a0609af605a74c473949628f2c6e
SuehtamCruz <matheus.cruz@mottu.com.br>
2021-12-30 15:11:22 -0300

    inicio do projeto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+import NotesComponent from './components/notes.component';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <main>
+      <form>
+      <input type="text" />
+
+      <textarea placeholder="Escreva sua nota aqu"></textarea>
+    </form>
+
+    <NotesComponent></NotesComponent>
+   </main>
   );
 }
 
---
+++ src/components/notes.component.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+export default class NotesComponent extends Component {
+  render() {
+    return (
+      <ul>
+        <li>
+          <section>
+            <h3>Título</h3>
+
+            <p>Escreva sua nota</p>
+          </section>
+        </li>
+        <li>
+          <section>
+            <h3>Título</h3>
+
+            <p>Escreva sua nota</p>
+          </section>
+        </li>
+      </ul>
+    );
+  }
+}
